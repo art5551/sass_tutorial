78b590e477a7ed6afc2bbfe6c910ec191e721dbe
art5551 <afmrp@protonmail.com>
2022-10-19 17:01:36 -0500

    Commit before chaging names of folders

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
 ###Rules
 
+To use this library in a new project.
+1.Drop the 
+
 When returning to this tutorial run gulp in terminal to reinstate the buildstyles and watchtask scripts
+
+###@debug will print output to terminal to debug sass
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('index.scss')
+  return src('shinobi/**/*.scss')
   .pipe(sass())
+  .pipe(purgecss({ content:['*.html'] }))
   .pipe(dest('css'))
 }
 
 function watchTask () {
-  watch(['index.scss'], buildStyles)
+  watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
